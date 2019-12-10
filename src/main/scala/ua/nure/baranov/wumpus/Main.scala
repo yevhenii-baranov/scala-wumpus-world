59123df187860ed48eb1d98d6e63b5bead082a60
Yevhenii Baranov <yevhenii.baranov@teamdev.com>
2019-12-10 12:58:35 +0200

    Add test layout.

--- src/main/scala/ua/nure/baranov/wumpus/Main.scala
+++ src/main/scala/ua/nure/baranov/wumpus/Main.scala
@@ -1,9 +1,16 @@
 package ua.nure.baranov.wumpus
 
 class Main {
+
+  val layout: String =
+    """
+      | P
+      |  G
+      | W  """.stripMargin
+
   def main(args: Array[String]): Unit = {
-    val environment = new Environment
-    val speleologist = new Speleologist
+    val environment = new Environment(layout)
     val navigator = new Navigator
+    val speleologist = new Speleologist(navigator, environment)
   }
 }
